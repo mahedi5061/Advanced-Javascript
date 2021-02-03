172fb9f2f3415ad6cab169e24bf801cf2687faed
mahedi5061 <mahedi5061@gmail.com>
2021-02-03 13:07:31 +0600

    first commit

---
+++ true-false.js
@@ -0,0 +1,23 @@
+//False
+//0
+//null
+//undefine
+//NaN
+//""
+//false normal false defiant value are false but when it will be declared as a string like 'false' it's value is true.
+
+//true
+//" "
+//"0"
+// - and + all values are true.
+//[]
+//{}
+//'false'
+
+const age=0;
+if(age || age==0){ //this condition is true because age=0 this value is false but age==0 this condition is true. so the result is true.
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is false');
+}
